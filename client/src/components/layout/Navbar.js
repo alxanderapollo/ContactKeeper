6555221c81583c687d53f1a8b2d32847b07ad31a
Alex <alexsalazarny@gmail.com>
2021-11-14 17:56:33 -0500

    Added nav links to home and about

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 //comes in as title and icon
 const  Navbar = ({title, icon}) => {
     return (
-        <div className="nanbar bg-primary">
+        <div className="navbar bg-primary">
             <h1>
-                
                 <i className={icon} />{title}
             </h1>
-            
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </div>
     )
 }
