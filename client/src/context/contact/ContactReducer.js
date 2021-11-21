3271c9a5f8bf6e4a8527cbb024625b432d2689b5
Alex <alexsalazarny@gmail.com>
2021-11-21 11:40:19 -0500

    Functionality with set, edit and delete now work

--- client/src/context/contact/ContactReducer.js
+++ client/src/context/contact/ContactReducer.js
@@ -1,42 +1,47 @@
 import {
-    ADD_CONTACT,
-    DELETE_CONTACT,
-    SET_CURRENT,
-    CLEAR_CURRENT,
-    UPDATE_CONTACT,
-    FILTER_CONTACTS,
-    CLEAR_FILTER,
-  } from "../types";
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
 
-  export default (state, action) =>{
-      switch(action.type){
-          case ADD_CONTACT:
-              return{
-                  ...state,
-                  contacts:[...state.contacts, action.payload]
-              }
-              //takes our state
-              //return and filter through our contacts -> filter function returns an array of contacts 
-              //this function will return an arra without the particular passed ID
-              case DELETE_CONTACT:
-              return{
-                  ...state,
-                  contacts:state.contacts.filter(contact => contact.id !== action.payload)
-              }
-              //Return state and contacts will be the passed payload
-              case SET_CURRENT:
-                return{
-                    ...state,
-                    contacts:action.payload
-                }
-                //contacts is set to null because we are reseting the state to an empty one
-                case CLEAR_CURRENT:
-                    return{
-                        ...state,
-                        contacts: null
-                    }
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
 
-          default:
-              return state;
-      }
+    //takes our state
+    //return and filter through our contacts -> filter function returns an array of contacts
+    //this function will return an arra without the particular passed ID
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+
+    //Return state and contacts will be the passed payload
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+
+    //contacts is set to null because we are resetting the state to an empty one
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+
+    default:
+      return state;
   }
+};
